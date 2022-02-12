c0f464aff635fb6922aeed1aa65a5bb3b37788c9
Kreozot <zzxyyz@gmail.com>
2022-02-12 23:25:03 +0300

    Перенос слов в ячейке названия

--- src/TransactionsTable/TransactionsTable.tsx
+++ src/TransactionsTable/TransactionsTable.tsx
@@ -33,7 +33,6 @@ import { CategoryCell } from './CategoryCell';
 import { MoneyCell } from './MoneyCell';
 
 import '@material/data-table/dist/mdc.data-table.css';
-import 'material-design-icons/iconfont/material-icons.css';
 import '@rmwc/data-table/data-table.css';
 import '@rmwc/icon/icon.css';
 
